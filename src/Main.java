8cec68bf98c5fbfae9417ee99fc72bff6468b150
Eduardo Calles <eduardo.calles@kodigo.org>
2024-09-03 20:48:52 -0600

    Otro comit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Welcome Java Developer 18 Team");
-        System.out.println("Git y Github practice");
+        System.out.println("Git y Github Practice");
     }
 }
